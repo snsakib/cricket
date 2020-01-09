148b510fcc0e02073bea5e2187ac91c5995a6f45
Syed Nazmus Sakib <contact@snsakib.com>
2020-01-09 13:08:37 +0600

    create home component layout

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
+  teams = [];
 
-  constructor() { }
-
-  ngOnInit() {
+  onSelectCountry(countryName) {
+    if (this.teams.length < 2) {
+      this.teams.push(countryName);
+    }
+    console.log(this.teams);
   }
 
+  setTeams() {
+    localStorage.setItem('team1', this.teams[0]);
+    localStorage.setItem('team2', this.teams[1]);
+  }
 }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button'; 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    MatGridListModule,
+    MatButtonModule
   ]
 })
-export class HomeModule { }
+export class HomeModule {
+}
