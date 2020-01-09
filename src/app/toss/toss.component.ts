343e95201e6304fa8f38718188641b5752da0fc7
Syed Nazmus Sakib <contact@snsakib.com>
2020-01-09 13:08:59 +0600

    create toss module and component

---
+++ src/app/toss/toss.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-toss',
+  templateUrl: './toss.component.html',
+  styleUrls: ['./toss.component.css']
+})
+export class TossComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/toss/toss.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TossComponent } from './toss.component';
+
+
+
+@NgModule({
+  declarations: [TossComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class TossModule { }
