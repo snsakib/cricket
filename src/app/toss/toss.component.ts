74064ba7a2b303d2e1f913c6d1a92be7e24e9736
Syed Nazmus Sakib <contact@snsakib.com>
2020-01-09 15:13:21 +0600

    update toss component

--- src/app/toss/toss.component.ts
+++ src/app/toss/toss.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-toss',
-  templateUrl: './toss.component.html',
-  styleUrls: ['./toss.component.css']
+  selector: "app-toss",
+  templateUrl: "./toss.component.html",
+  styleUrls: ["./toss.component.css"]
 })
 export class TossComponent implements OnInit {
+  teams = [localStorage.getItem("team1"), localStorage.getItem("team2")];
+  battingTeam;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  onSelectTeam($event: Event) {
+    this.battingTeam = event.target.value;
   }
 
+  startMatch() {
+    this.router.navigate(["/play", this.battingTeam]);
+  }
+
+  ngOnInit() {}
 }
--- src/app/toss/toss.module.ts
+++ src/app/toss/toss.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TossComponent } from './toss.component';
-
-
+import {MatRadioModule} from '@angular/material/radio';
+import {MatButtonModule} from '@angular/material/button'; 
 
 @NgModule({
   declarations: [TossComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatRadioModule,
+    MatButtonModule
   ]
 })
 export class TossModule { }
