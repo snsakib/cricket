6d3057486c4b103776e7e904bfb9d48c7415a750
Syed Nazmus Sakib <contact@snsakib.com>
2020-01-09 12:33:14 +0600

    add flags

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
-
-
+import {MatGridListModule} from '@angular/material/grid-list';
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatGridListModule,
   ]
 })
 export class HomeModule { }
